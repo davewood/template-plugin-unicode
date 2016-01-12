13abccd5c95cdaeabf50e0bde64c03df7ecf201b
David Schmidt <mail@davidschmidt.at>
2016-01-12 10:35:06 +0100

    POD fixes

--- lib/Template/Plugin/Unicode.pm
+++ lib/Template/Plugin/Unicode.pm
@@ -4,9 +4,10 @@ use strict;
 use warnings;
 use base qw/Template::Plugin/;
 
-# ABSTRACT: unicode chars via codepoint
+# ABSTRACT: insert characters via unicode codepoints.
 
 =head1 SYNOPSIS
+
     use Template;
     my $tt = Template->new;
     my $template = '[% USE Unicode %][% Unicode.codepoint2char('0x263A') %]';
@@ -16,16 +17,17 @@ use base qw/Template::Plugin/;
     say $smiley;
 
 =head1 DESCRIPTION
-Insert characters into your text via unicode codepoint.
+
+Insert characters via unicode codepoints.
 
 =head1 SEE ALSO
 
 Another way of inserting characters via unicode codepoints is by
-adding a sub ref to the \%vars hashres passed to process().
+adding a sub ref to the \%vars hashref passed to process().
 
     my $u    = sub { chr(hex($_[0])) };
     my $text = '[% u('0x263a') %]';
-    process(\$text, { u => sub {} }, $output, %options)
+    process(\$text, { u => sub {} }, $output);
 
 =cut
 
